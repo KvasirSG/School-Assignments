982fbfc71ecf2e166dd1cf22ddb7a7b9f87fddf1
KvasirSG <kennethheimann@gmail.com>
2016-01-08 11:16:42 +0100

    Create valuta_Converter.cs

---
+++ valuta_Converter.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Case.valutaberegner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Beløb_DKK = 0;
+            double Beløb_USD = 0;
+            double Beløb_Euro = 0;
+            string valg;
+
+            Console.Write("Valg af converter \n \n1. convert DKK. \n2. convert USD. \n3. convert Euro. \n\n");
+            valg = Console.ReadLine();
+            switch (valg)
+            {
+                case "1":
+                    
+                    Console.WriteLine("");
+
+
+                    break;
+
+                case "2":
+
+
+
+                    break;
+
+                case "3":
+
+
+                    break;
+            }
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+        
+        
+    }
+}
