bcd47ed173e459c44008a220d9c3ef5406661d96
Frumze <frederik.nw@live.dk>
2016-01-13 10:22:12 +0100

    Update valuta_Converter.cs

--- valuta_Converter.cs
+++ valuta_Converter.cs
@@ -15,31 +15,49 @@ namespace Case.valutaberegner
     {
         static void Main(string[] args)
         {
-            //Variables
-            double Beløb_DKK = 0;
-            double Beløb_USD = 0;
-            double Beløb_Euro = 0;
+           double Beløb_DKK = 0, Beløb_USD = 0, Beløb_Euro = 0;
+
             string valg;
             
-            //code
-            Console.Write("Valg af converter \n \n1. convert DKK. \n2. convert USD. \n3. convert Euro. \n\n");
+
+
+            Console.Write("Valg af converter \n \n1. convert DKK til USD og Euro. \n2. convert USD til DKK og Euro. \n3. convert Euro til DKK og USD. \n\n");
             valg = Console.ReadLine();
             switch (valg)
             {
                 case "1":
                     
-                    Console.WriteLine("");
+                    Console.WriteLine("Hvor mange penge skal conveteres");
+                    Beløb_DKK = double.Parse(Console.ReadLine());
+                    Beløb_Euro = Beløb_DKK * 0.134;
+                    Console.WriteLine(Beløb_Euro + ":" + "EUR");
 
+                    Beløb_USD = Beløb_DKK * 0.146;
+                    Console.WriteLine(Beløb_USD + ":"+ "$");
 
                     break;
 
                 case "2":
+                    Console.WriteLine("Hvor mange penge skal der conveteres");
+                    Beløb_USD = double.Parse(Console.ReadLine());
+                    Beløb_DKK = Beløb_USD * 6.8;
+                    Console.WriteLine(Beløb_DKK + ":" + "DKK");
+
+                    Beløb_Euro = Beløb_USD * 0.91;
+                    Console.WriteLine(Beløb_Euro + ":" + "EUR");
 
 
 
                     break;
 
                 case "3":
+                    Console.WriteLine("Hvor mange penge skal converteres");
+                    Beløb_Euro = double.Parse(Console.ReadLine());
+                    Beløb_DKK = Beløb_Euro * 7.4;
+                    Console.WriteLine(Beløb_DKK + ":" + "DKK");
+
+                    Beløb_USD = Beløb_Euro * 1.08;
+                    Console.WriteLine(Beløb_USD + ":" + "$");
 
 
                     break;
